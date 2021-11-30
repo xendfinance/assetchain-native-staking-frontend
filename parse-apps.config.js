4f756f8626df7c1f72c9a01ea16bf6bdd2149419
franko4don <franko4don@yahoo.com>
2021-11-30 01:54:19 -0600

    changed deployment script

---
+++ parse-apps.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+        name        : 'StakeUI',
+        script      : 'npm start',
+        watch       : false,
+        merge_logs  : true,
+        cwd         : '/var/www/teststake.xend.finance',
+    }]
+};
