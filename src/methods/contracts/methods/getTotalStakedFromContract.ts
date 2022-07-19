4dff73c0b78a4bb793a409d24a749a27954115ee
kay albertus <kayalbertus@gmail.com>
2022-07-19 09:38:50 +0200

    Updated Total Tokens Staked In Protocol value

--- src/methods/contracts/methods/getTotalStakedFromContract.ts
+++ src/methods/contracts/methods/getTotalStakedFromContract.ts
@@ -6,7 +6,7 @@ async function GetTotalStakedFromContract() {
        
         const xendContract = await createContractPreConnect(abiManager.XSTAKING, "0x3d4D0699C4Df1539Fdc42C6F9594A478c6929051");
       
-        return  await xendContract.methods.totalStakedToken().call();
+        return  await xendContract.methods.getTotalTokenStakesInContract().call();
 
     } catch (err :any) {
         console.log(err);
